cce7caed1e18cf17d01ea7c98c171dd897d0021a
decentsimple <decentsimple@yahoo.com.ph>
2022-12-24 21:54:24 +0800

    playwright start

---
+++ playwright/src/main/java/projects/playwright/App.java
@@ -0,0 +1,13 @@
+package projects.playwright;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
